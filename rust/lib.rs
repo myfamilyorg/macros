6364f0104baf696c7a2bcb83ebc483efb51b6df2
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-08 12:19:38 -0700

    add to prelude

--- rust/lib.rs
+++ rust/lib.rs
@@ -4,7 +4,7 @@ pub extern crate ffi;
 
 pub mod prelude {
     pub use super::ffi;
-    pub use super::{aadd, aload, astore, asub, cas, try_box_slice};
+    pub use super::{aadd, aload, astore, asub, cas, exit, try_box_slice};
 }
 
 #[macro_export]
